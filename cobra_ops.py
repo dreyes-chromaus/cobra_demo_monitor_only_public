0b88432dfbda55816d286e0aa06d29fab0a3d1a6
Deangelo Reyes <dreyes@chromaus.com>
2025-09-11 16:10:54 -0700

    added timeout to cobra_function_keys.py

--- cobra_ops.py
+++ cobra_ops.py
@@ -72,7 +72,7 @@ class cobra_demo:
         current_cobra = self.get_cobra_head(head_select)
         for command in command_list:
             if command in functions and command is not "Port":
-                if command_list[command] is not None:
+                if command_list[command][0] is not None:
                     cobra_feedback = getattr(current_cobra, command)(command_list[command][0])
                     time.sleep(command_list[command][1])
                 else:
